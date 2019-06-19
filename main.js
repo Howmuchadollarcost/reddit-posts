3c8200a14fdbedf1079850f2882ea82f5d56befa
Howmuchadollarcost <muluyonas@yahoo.com>
2019-06-19 17:14:49 +0200

    added lazy load

--- main.js
+++ main.js
@@ -1,54 +1,73 @@
-const results = document.getElementById('results');
-const loading = document.getElementById('loading');
-const bpt = document.getElementById('bpt');
-const mi = document.getElementById('mi');
+const results = document.getElementById("results");
+const loading = document.getElementById("loading");
+const bpt = document.getElementById("bpt");
+const mi = document.getElementById("mi");
 
+loading.style.display = "none";
 
-loading.style.display = 'none';
+bpt.addEventListener("click", () => {
+  const API = `https://www.reddit.com/r/blackpeopletwitter/.json`;
+  getPics(API);
+  results.innerHTML = "";
+});
 
-bpt.addEventListener('click', ()=>{
-    const API = `https://www.reddit.com/r/blackpeopletwitter/.json`;
-    getPics(API);
-    results.innerHTML='';
-})
+mi.addEventListener("click", () => {
+  const API = `https://www.reddit.com/r/mildlyinteresting/.json`;
+  getPics(API);
+  results.innerHTML = "";
+});
 
-mi.addEventListener('click', ()=>{
-    const API = `https://www.reddit.com/r/mildlyinteresting/.json`;
-    getPics(API);
-    results.innerHTML='';
-})
+async function getPics(API) {
+  loading.style.display = "";
+  const res = await fetch(API);
+  const data = await res.json();
+  const dataArr = [];
+  showPics(data.data.children);
 
+  let lazyImages = [...document.querySelectorAll(".post")];
+  let inAdvance = 300;
 
-async function getPics(API){
-    loading.style.display = '';
-    const res = await fetch(API);
-    const data = await res.json();
-    const dataArr = [];
-    dataArr.push(data.data.children);
-    dataArr[0].shift();   //Remove modes post
-    showPics(dataArr);
-};
-
-
-const showPics= (data) => {
-    data[0].map(item =>{
-        let img = document.createElement('img');
-        img.src = item.data.url;
-        let title = document.createElement('h4');
-        title.innerHTML = item.data.title;
-        let div = document.createElement('div');
-        div.className = "post";
-
-        div.appendChild(title);
-        div.appendChild(img);
-
-        results.appendChild(div);
-        loading.style.display = 'none';
-    })
+  function lazyLoad() {
+    lazyImages.forEach(image => {
+      if (
+        image.offsetTop <
+        window.innerHeight + window.pageYOffset + inAdvance
+      ) {
+        image.src = image.dataset.src;
+        image.onload = () => image.classList.add("loaded");
+      }
+    });
+  }
+  lazyLoad();
+  window.addEventListener("scroll", _.throttle(lazyLoad, 16));
+  window.addEventListener("resize", _.throttle(lazyLoad, 16));
 }
 
+const showPics = data => {
+  data.map(item => {
+    const urls = item.data.preview.images[0].source.url;
+    const newUrl = urls.replace(/amp;/gi, "");
+    let img = document.createElement("img");
+    img.src = newUrl;
+    img.className = "blurry";
+    let title = document.createElement("h4");
+    title.innerHTML = item.data.title;
+    let div = document.createElement("div");
+    div.className = "post";
 
- 
+    div.appendChild(title);
+    div.appendChild(img);
 
+    results.appendChild(div);
+    loading.style.display = "none";
 
- 
+    let timeout = this.setTimeout(function() {
+      showImage();
+    }, 1500);
+
+    const showImage = () => {
+      img.style.filter = "blur(0px)";
+      console.log("hello");
+    };
+  });
+};
